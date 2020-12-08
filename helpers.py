b32f451f94555567c9d7c9629df42731ce807e61
Alex Inskeep <ainskeep@cradlepoint.com>
2020-12-08 12:57:33 -0700

    Add group split helper

--- helpers.py
+++ helpers.py
@@ -6,3 +6,20 @@ def read_input(file_name=IN_FILE):
     with open(file_name, 'r') as infile:
         lines = infile.read().splitlines()
     return lines
+
+
+def blank_line_group(lines):
+    grouped_lines = []
+
+    tmp_group = []
+    for line in lines:
+        if line:
+            tmp_group.append(line)
+        else:
+            grouped_lines.append(tmp_group)
+            tmp_group = []
+
+    # add the last group
+    grouped_lines.append(tmp_group)
+
+    return grouped_lines
